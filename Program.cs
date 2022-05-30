10bf27a3f6de112a76df8929e1e9a5f1e8d61ede
vamsipentakota <103921699+vamsipentakota@users.noreply.github.com>
2022-05-30 15:42:11 +0530

    javascript assignment1

--- Program.cs
+++ Program.cs
@@ -4,45 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace jsassignments
 {
-    public delegate void calculator(int a, int b);
-
-
-
     class Program
     {
-        public static void addition(int a, int b)
-        {
-            int res = a + b;
-            Console.WriteLine($"Addition result is {res}");
-        }
-        public static void substraction(int a, int b)
-        {
-            int res = a - b;
-            Console.WriteLine($"substraction result is {res}");
-        }
-        public static void multiplication(int a, int b)
-        {
-            int res = a * b;
-            Console.WriteLine($" multiplicatio result is {res}");
-        }
-        public static void division(int a, int b)
-        {
-            int res = a / b;
-            Console.WriteLine($" division result is {res}");
-        }
         static void Main(string[] args)
         {
-            calculator obj = new calculator(Program.addition);
-            obj.Invoke(10, 20);
-            obj = substraction;
-            obj(20, 10);
-            obj = multiplication;
-            obj(30, 40);
-            obj = division;
-            obj(50, 10);
-            Console.ReadLine();
         }
     }
 }
