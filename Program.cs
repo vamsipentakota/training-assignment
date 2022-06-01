6c46a58d8e64c3802ac33cd9c775b23052b1fc28
vamsipentakota <103921699+vamsipentakota@users.noreply.github.com>
2022-06-01 16:02:42 +0530

    codebase3exam

--- Program.cs
+++ Program.cs
@@ -3,46 +3,86 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
-namespace ConsoleApp1
+namespace codebase3
 {
-    public delegate void calculator(int a, int b);
-
-
 
     class Program
     {
-        public static void addition(int a, int b)
+        public static SqlConnection con;
+        public static SqlCommand cmd;
+        public static SqlDataReader dr;
+
+        public static SqlConnection getconnection()
         {
-            int res = a + b;
-            Console.WriteLine($"Addition result is {res}");
+            con = new SqlConnection("data source=RMGW58ZLPC0962\\SQLEXPRESS;Initial Catalog=Assignment1DB;user id=sa;password=Temp1234");
+            con.Open();
+            return con;
         }
-        public static void substraction(int a, int b)
+
+        public static void insertEmployee()
         {
-            int res = a - b;
-            Console.WriteLine($"substraction result is {res}");
+            try
+            {
+                con = getconnection();
+                Console.WriteLine("Enter the Employee name : ");
+                string ename = Console.ReadLine();
+                Console.WriteLine("Enter the Employee Salary : ");
+                float esal = Convert.ToSingle(Console.ReadLine());
+                Console.WriteLine("Enter the Employee type (C or P) : ");
+                string etype = Console.ReadLine();
+                cmd = new SqlCommand("AddEmployee", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd = new SqlCommand("execute AddEmployee @ename,@esal,@etype", con);
+                cmd.Parameters.Add(new SqlParameter("@ename", ename));
+                cmd.Parameters.Add(new SqlParameter("@esal", esal));
+                cmd.Parameters.Add(new SqlParameter("@etype", etype));
+
+
+                int res = cmd.ExecuteNonQuery();
+                if (res > 0)
+                {
+                    Console.WriteLine("rows effected :{0}", res);
+                }
+                else
+                {
+                    Console.WriteLine("No Row Effected");
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
-        public static void multiplication(int a, int b)
+        public static void DisplayAllEmployee()
         {
-            int res = a * b;
-            Console.WriteLine($" multiplicatio result is {res}");
-        }
-        public static void division(int a, int b)
-        {
-            int res = a / b;
-            Console.WriteLine($" division result is {res}");
+            con = getconnection();
+
+
+            cmd = new SqlCommand("select * from Code_Employee", con);
+
+
+
+            dr = cmd.ExecuteReader();
+
+            while (dr.Read())
+            {
+                Console.WriteLine("Emp Id is : " + dr[0]);
+                Console.WriteLine("Emp name is : " + dr[1]);
+                Console.WriteLine("Emp Salary is : " + dr[2]);
+                Console.WriteLine("Emp Type is : " + dr[3]);
+                Console.WriteLine("--------------------------------------");
+
+            }
         }
         static void Main(string[] args)
         {
-            calculator obj = new calculator(Program.addition);
-            obj.Invoke(10, 20);
-            obj = substraction;
-            obj(20, 10);
-            obj = multiplication;
-            obj(30, 40);
-            obj = division;
-            obj(50, 10);
-            Console.ReadLine();
+            insertEmployee();
+            DisplayAllEmployee();
+            Console.Read();
         }
     }
+
 }
